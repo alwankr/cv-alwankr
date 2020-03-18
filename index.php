bda277a29cefc67b2d7f2983ad8138c4d537b2c6
alwankr <53940370+alwankr@users.noreply.github.com>
2020-03-18 13:48:28 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+
+  <title>Hello, world!</title>
+</head>
+
+<body>
+
+  <div class="container-fluid">
+    <div class="row bg-primary">
+      <div class="col-md-12">
+        <h2 class="text-center p-2 text-white">My Personal Information</h2>
+      </div>
+    </div>
+
+    <div class="row img-bac">
+      <div class="col-md-12">
+        <div class="text-center text-white" style="padding: 100px">
+          <img class="rounded-circle" style="width: 200px; height: 200px" src="https://storage.googleapis.com/alwan-bucket-console/IMG_20190829_122716.jpeg" alt="profile">
+          <h4 class="mt-2">Jhon Doe</h4>
+          <p>Junior Web Developer</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2 bg-success" style="height: 5px"></div>
+      <div class="col-md-4 bg-primary" style="height: 5px"></div>
+      <div class="col-md-4 bg-info" style="height: 5px"></div>
+      <div class="col-md-2 bg-danger" style="height: 5px"></div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-6">
+        <div class="pt-5" style="padding-left: 200px">
+          <h4>My Story</h4>
+          <p class="pt-3">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+          </p>
+          <p>
+            survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="pt-5" style="padding-right: 200px">
+          <h4>Personal information</h4>
+          <p class="pt-3">Age <strong>23 Years old</strong></p>
+          <div class="border-bottom border-dark"></div>
+          <p class="pt-3">Phone: <strong>+62-8131-1351-xxx</strong></p>
+          <div class="border-bottom border-dark"></div>
+          <p class="pt-3">Email: <strong>jhon@dicoding.com</strong></p>
+          <div class="border-bottom border-dark"></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row mt-5">
+      <div class="col-md-12 text-center">
+        <h4>Find me in!</h4>
+      </div>
+    </div>
+  </div>
+
+  <footer class="footer text-center mt-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <a href="https://www.facebook.com/alwan18" target="blank">
+            <img src="assets/icon/facebook.svg" alt="facebook">
+          </a>
+        </div>
+        <div class="col-md-4">
+          <a href="https://twitter.com/alwan_rahman" target="blank">
+            <img src="assets/icon/twitter.svg" alt="twitter">
+          </a>
+        </div>
+        <div class="col-md-4">
+          <a href="http://alwankamarulrahman.blogspot.com/" target="blank">
+            <img src="assets/icon/blogger.svg" alt="blogspot">
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
